6dbfcb43f1423942c08f5a640127e1c5b48acd98
Hugues Lepesant <hugues@lepesant.com>
2018-07-05 08:55:12 +0200

    clear

--- README.md
+++
@@ -1,4 +0,0 @@
-proftpadmin
-===========
-
-A Symfony project created on May 3, 2018, 1:44 pm.
--- src/ProftpBundle/ProftpBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace ProftpBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class ProftpBundle extends Bundle
-{
-}
